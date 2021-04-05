e487e1195e385ff5cfc7b0b7d1c5df643fffea7f
Princess Maniclang <princessmaniclang@gmail.com>
2021-04-05 21:03:39 +0800

    Section 5 Ch.48

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -22,7 +22,7 @@ namespace API.Extensions
                     };
                 });
 
-            return services
+            return services;
         }
     }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import {HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
