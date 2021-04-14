2da57c10ab23ca73fd2899ff0f5d71949ea37359
Princess Maniclang <princessmaniclang@gmail.com>
2021-04-14 18:08:28 +0800

    Section 5 up to Ch 59

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,11 +13,10 @@ export class AppComponent implements OnInit {
   users: any;
 
 /*Dependency Injection Example*/
-constructor(private http:HttpClient, private accountService: AccountService){}
+constructor(private accountService: AccountService){}
 
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -25,14 +24,7 @@ constructor(private http:HttpClient, private accountService: AccountService){}
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
   }
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    },error => {
-      console.log(error);
-    })
-
-  }
+  
 }
 
 
