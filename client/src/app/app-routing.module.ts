2c75b1699eb451b0095d440cd67d70591efd0d72
Princess Maniclang <princessmaniclang@gmail.com>
2021-05-05 17:24:24 +0800

    Section 6 up to Chapter 66

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent}, //when user browse in 4002 the component will be loaded is the HomeComponent
+  {path: 'members', component: MemberListComponent},
+  {path: 'members/:id', component: MemberDetailComponent},
+  {path: 'lists', component: ListsComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: '**', component: HomeComponent,pathMatch: 'full'}, //wild card roots user type something that is not match above it will be directed to HomeComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
